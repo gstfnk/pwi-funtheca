883129ae7f60031479ff88a4e07f9a427d75be5a
gstfnk <gabrysia.stefaniak@gmail.com>
2022-06-10 23:52:31 +0200

    fix navbar from bootstrap 4.1 to 5.1

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,13 +1,10 @@
 $('document').ready(function () {
-
-    $('.table #deleteButton').on('click',function(event) {
+    $('.table #deleteButton').on('click', function (event) {
         event.preventDefault();
         var href = $(this).attr('href');
         $('#deleteModal #delRef').attr('href', href);
         $('#deleteModal').modal();
-
     });
-
 });
 
 
