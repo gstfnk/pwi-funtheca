61e69e7378ba53d63d618d536ab487a6c5a06899
gstfnk <gabrysia.stefaniak@gmail.com>
2022-06-21 21:19:06 +0200

    add random quotes feature

--- src/main/resources/static/js/langs.js
+++ src/main/resources/static/js/langs.js
@@ -5,11 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const bookListEl = document.querySelector('.bookListLANG');
     const quoteEl = document.querySelector('.quoteLANG');
+    const playMemoryEl = document.querySelector('.playMemoryLANG');
     const techEl = document.querySelector('.techLANG');
     const langsEl = document.querySelector('.langsLANG');
     const plEl = document.querySelector('.plLANG');
     const enEl = document.querySelector('.enLANG');
     const shareBookEl = document.querySelector('.shareBookLANG');
+    const shareBookDesc = document.querySelector('.shareBookLANGdesc');
     const addBookEl = document.querySelector('.addBookLANG');
     const titleEl = document.querySelector('.titleLANG');
     const authorEl = document.querySelector('.authorLANG');
@@ -27,8 +29,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const deleteEl2 = document.querySelector('.deleteLANG2');
     const exitEl2 = document.querySelector('.exitLANG2');
 
+    const bookListEl2 = document.querySelector('.bookListLANG2');
+    const quoteEl2 = document.querySelector('.quoteLANG2');
+    const memoryGameEl = document.querySelector('.memoryGameLANG');
+
+    const bookListDescEl = document.querySelector('.bookListDescLANG');
+    const quoteDescEl = document.querySelector('.quoteDescLANG');
+    const memoryDescEl = document.querySelector('.memoryDescLANG');
+
     link.forEach(el => {
-        el.addEventListener('click',() => {
+        el.addEventListener('click', () => {
             langEl.querySelector('.activeLang').classList.remove('activeLang');
             el.classList.add('activeLang');
 
@@ -37,12 +47,36 @@ document.addEventListener('DOMContentLoaded', () => {
             bookListEl.textContent = langs[attr].bookListLANG;
             techEl.textContent = langs[attr].techLANG;
             quoteEl.textContent = langs[attr].quoteLANG;
+            playMemoryEl.textContent = langs[attr].playMemoryLANG;
             langsEl.textContent = langs[attr].langsLANG;
+            plEl.textContent = langs[attr].plLANG;
+            enEl.textContent = langs[attr].enLANG;
 
-
+            shareBookEl.textContent = langs[attr].shareBookLANG;
+            shareBookDesc.textContent = langs[attr].shareBookLANGdesc;
+            addBookEl.textContent = langs[attr].addBookLANG;
+            titleEl.textContent = langs[attr].titleLANG;
+            authorEl.textContent = langs[attr].authorLANG;
+            deleteEl.textContent = langs[attr].deleteLANG;
+            newBookEl.textContent = langs[attr].newBookLANG;
 
             titlePlaceholderEl.placeholder = langs[attr].titleLANG;
             authorPlaceholderEl.placeholder = langs[attr].authorLANG;
+
+            exitEl.textContent = langs[attr].exitLANG;
+            saveEl.textContent = langs[attr].saveLANG;
+            confirmDeleteEl.textContent = langs[attr].confirmDeleteLANG;
+            questionDeleteEl.textContent = langs[attr].questionDeleteLANG;
+            deleteEl2.textContent = langs[attr].deleteLANG;
+            exitEl2.textContent = langs[attr].exitLANG;
+
+            bookListEl2.textContent = langs[attr].bookListLANG;
+            quoteEl2.textContent = langs[attr].quoteLANG;
+            memoryGameEl.textContent = langs[attr].memoryGameLANG;
+
+            bookListDescEl.textContent = langs[attr].bookListDescLANG;
+            quoteDescEl.textContent = langs[attr].quoteDescLANG;
+            memoryDescEl.textContent = langs[attr].memoryDescLANG;
         });
     });
 
@@ -50,42 +84,52 @@ document.addEventListener('DOMContentLoaded', () => {
         "english":
             {
                 "bookListLANG": "Book list",
-                "techLANG" : "Technologies",
-                "quoteLANG" : "Quote",
-                "playMemoryLANG" : "Play memory game!",
-                "langsLANG" : "Languages",
-                "plLANG" : "Polish",
-                "enLANG" : "English",
-                "shareBookLANG" : "Share your favorite book!",
-                "addBookLANG" : "Add book",
-                "titleLANG" : "Title",
-                "authorLANG" : "Author",
-                "deleteLANG" : "Delete",
-                "newBookLANG" : "New book",
-                "exitLANG" : "Close",
-                "saveLANG" : "Save",
-                "confirmDeleteLANG" : "Confirm the deletion",
-                "questionDeleteLANG" : "Do you want to delete this book?"
+                "techLANG": "Technologies",
+                "quoteLANG": "Quote",
+                "playMemoryLANG": "Play memory game!",
+                "langsLANG": "Languages",
+                "plLANG": "Polish",
+                "enLANG": "English",
+                "shareBookLANG": "Share your favorite book!",
+                "addBookLANG": "Add book",
+                "titleLANG": "Title",
+                "authorLANG": "Author",
+                "deleteLANG": "Delete",
+                "newBookLANG": "New book",
+                "exitLANG": "Close",
+                "saveLANG": "Save",
+                "memoryGameLANG": "Memory Game",
+                "confirmDeleteLANG": "Confirm the deletion",
+                "questionDeleteLANG": "Do you want to delete this book?",
+                "memoryDescLANG": "Test yourself and your memory in a simple game that our website has prepared for you!",
+                "quoteDescLANG": "By clicking the \"Quote\" button on the top bar of the page with another sentence, check if you can read something for yourself!",
+                "bookListDescLANG": "Do you feel the chills after reading the book? Or maybe one of them put a smile on your face? Share the title and its author in our book list!",
+                "shareBookLANGdesc": "Sharing the positive is fun, so share what has caught your eye recently from the world of the magic book :)"
             },
         "polish":
             {
                 "bookListLANG": "Spis książek",
-                "techLANG" : "Technologie",
-                "quoteLANG" : "Cytat",
-                "playMemoryLANG" : "Zagraj w memory!",
-                "langsLANG" : "Języki",
-                "plLANG" : "Polski",
-                "enLANG" : "Angielski",
-                "shareBookLANG" : "Podziel się swoją ulubioną książką!",
-                "addBookLANG" : "Dodaj książkę",
-                "titleLANG" : "Tytuł",
-                "authorLANG" : "Autor",
-                "deleteLANG" : "Usuń",
-                "newBookLANG" : "Nowa książka",
-                "exitLANG" : "Zamknij",
-                "saveLANG" : "Zapisz",
-                "confirmDeleteLANG" : "Potwierdź usunięcie",
-                "questionDeleteLANG" : "Na pewno chcesz usunąć książkę?"
+                "techLANG": "Technologie",
+                "quoteLANG": "Cytat",
+                "playMemoryLANG": "Zagraj w memory!",
+                "langsLANG": "Języki",
+                "plLANG": "Polski",
+                "enLANG": "Angielski",
+                "shareBookLANG": "Podziel się swoją ulubioną książką!",
+                "addBookLANG": "Dodaj książkę",
+                "titleLANG": "Tytuł",
+                "authorLANG": "Autor",
+                "deleteLANG": "Usuń",
+                "newBookLANG": "Nowa książka",
+                "exitLANG": "Zamknij",
+                "saveLANG": "Zapisz",
+                "memoryGameLANG": "Gra Memory",
+                "confirmDeleteLANG": "Potwierdź usunięcie",
+                "questionDeleteLANG": "Na pewno chcesz usunąć książkę?",
+                "memoryDescLANG": "Sprawdź siebie i swoją pamięć w prostej grze, jaką przygotowała Ci nasza strona!",
+                "quoteDescLANG": "Klikając w przycisk \"Cytat\" umieszczony na górnym pasku strony inną sentencję, sprawdź, czy przeczytasz coś dla siebie!",
+                "bookListDescLANG": "Czujesz dreszcze po przeczytaniu książki? Albo może któraś z nich sprawiła uśmiech na Twojej twarzy? Podziel się tytułem i jej autorem w naszym spisie książek!",
+                "shareBookLANGdesc": "Dzielenie się tym co pozytywne jest fajne, więc podziel się tym, co ostatnio wpadło Ci w oko ze świata magicznej książki :)"
             }
     };
 });
--- src/main/resources/static/js/quotesRandom.js
+++ src/main/resources/static/js/quotesRandom.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const authorOUT = document.getElementById('authorRandom');
     const citeOUT = document.getElementById('citeRandom');
     let randomNumber;
+    let previousNumber;
     let attr = document.querySelector('.activeLang').getAttribute('language');
 
     link.forEach(el => {
@@ -13,12 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (attr === "polish") {
                 quoteOUT.textContent = quotesPL[randomNumber];
-                authorOUT.textContent = author[randomNumber];
+                authorOUT.textContent = authorPL[randomNumber];
                 citeOUT.textContent = citePL[randomNumber];
             }
             if (attr === "english") {
                 quoteOUT.textContent = quotesEN[randomNumber];
-                authorOUT.textContent = author[randomNumber];
+                authorOUT.textContent = authorEN[randomNumber];
                 citeOUT.textContent = citeEN[randomNumber];
             }
         });
@@ -26,46 +27,105 @@ document.addEventListener('DOMContentLoaded', () => {
 
     quoteLang.addEventListener('click', () => {
         randomNumber = Math.floor(Math.random() * quotesPL.length);
+        while (randomNumber === previousNumber) {
+            randomNumber = Math.floor(Math.random() * quotesPL.length);
+        }
 
         if (attr === "polish") {
             quoteOUT.innerHTML = quotesPL[randomNumber];
-            authorOUT.textContent = author[randomNumber];
-            citeOUT.textContent = citePL[randomNumber];
+            authorOUT.innerHTML = authorPL[randomNumber];
+            citeOUT.innerHTML = citePL[randomNumber];
         }
         if (attr === "english") {
             quoteOUT.innerHTML = quotesEN[randomNumber];
-            authorOUT.textContent = author[randomNumber];
-            citeOUT.textContent = citeEN[randomNumber];
+            authorOUT.innerHTML = authorEN[randomNumber];
+            citeOUT.innerHTML = citeEN[randomNumber];
         }
+
+        previousNumber = randomNumber;
     });
 
     const quotesPL = [
         "Reszta jest milczeniem.",
         "Nigdy nie rezygnuj z tego co chcesz naprawdę w życiu robić. Osoba z wielkimi marzeniami jest potężniejsza niż osoba posiadająca same suche fakty.",
-        "Słuchaj swojego serca. Ono wie wszystko."
+        "Słuchaj swojego serca. Ono wie wszystko.",
+        "Proście, a będzie wam dane, szukajcie, a znajdziecie.",
+        "Ważne, że żyjesz w zgodzie ze sobą, nikogo nie udajesz i przed niczym nie uciekasz. Kiedy jesteś świadomie sobą i kochasz ten stan – jesteś szczęśliwy.",
+        "Wszyscy od czasu do czasu bywamy niemili. Wszyscy robimy rzeczy, które bardzo chcielibyśmy cofnąć. Te żale stają się częścią tego, kim jesteśmy, wraz ze wszystkim innym. Jeśli ktoś próbuje to zmienić, to tak, jakby chciał gonić chmury.",
+        "Często wystarczy jedna chwila, by wszystko zmienić.",
+        "Przeszłość nie chce, żeby ją zmieniać. Przeszłość jest nieustępliwa.",
+        "Nadchodzą trudne czasy. Trudne i niebezpieczne. Nadchodzi czas zmian. Przykro byłoby starzeć się w przekonaniu, że nie uczyniło się niczego, by zmiany, które nadchodzą, były zmianami na lepsze. Prawda?",
+        "Dobrze widzi się tylko sercem. Najważniejsze jest niewidoczne dla oczu.",
+        "- A jeśli pewnego dnia będę musiał odejść? - spytał Krzyś, ściskając Misiową łapkę. - Co wtedy?\n- Nic wielkiego. - zapewnił go Puchatek. - Posiedzę tu sobie i na Ciebie poczekam. Kiedy się kogoś kocha, to ten drugi ktoś nigdy nie znika."
     ]
 
     const quotesEN = [
         "The rest is silence.",
         "Never give up on what you really want to do. The person with big dreams is more powerful than one with all the facts.",
-        "Listen to your heart. It knows all things."
+        "Listen to your heart. It knows all things.",
+        "Ask, and it shall be given to you; seek, and you shall find.",
+        "It’s important to live according to what you feel is right, without pretending you’re somebody else and running away from something. When you live like this and you enjoy it, that’s when you’re happy.",
+        "We are all rude from time to time. We all do things that we'd love to take back. These regrets become part of who we are, along with everything else. If someone tries to change that, it's like chasing the clouds.",
+        "Often one moment is enough to change everything",
+        "The past does not want to be changed. The past is relentless.",
+        "Times are hard. Difficult and dangerous. The time of change is coming. It would be sad to grow old believing that nothing has been done to make the changes that are coming change for the better. Truth?",
+        "You only see well with your heart. The most important is invisible for eyes",
+        "\"What if one day I have to leave?\" - Krzyś asked, clutching Teddy's paw. - Then what?\n- Nothing big. Pooh assured him. - I will sit here and wait for you. When you love someone, the other person never disappears."
     ]
 
-    const author = [
+    const authorPL = [
         "William Shakespeare",
         "Albert Einstein",
-        "Paulo Coelho"
+        "Paulo Coelho",
+        "św. Mateusz",
+        "Beata Pawlikowska",
+        "Libba Bray",
+        "Janette Rallison",
+        "Stephen King",
+        "Andrzej Sapkowski",
+        "Antoine de Saint-Exupery",
+        "Alan Alexander Milne"
+    ]
+
+    const authorEN = [
+        "William Shakespeare",
+        "Albert Einstein",
+        "Paulo Coelho",
+        "st. Matthew",
+        "Beata Pawlikowska",
+        "Libba Bray",
+        "Janette Rallison",
+        "Stephen King",
+        "Andrzej Sapkowski",
+        "Antoine de Saint-Exupery",
+        "Alan Alexander Milne"
     ]
 
     const citePL = [
         "Hamlet",
         "Z życia",
-        "Alchemik"
+        "Alchemik",
+        "Biblia",
+        "W dżungli samotności",
+        "Mroczny sekret",
+        "Taka sobie wróżka",
+        "Dallas '63",
+        "Krew Elfów",
+        "Mały Książę",
+        "Kubuś Puchatek"
     ]
 
     const citeEN = [
         "The Tragedy of Hamlet",
         "From life",
-        "The Alchemist"
+        "The Alchemist",
+        "Bible",
+        "In the jungle of loneliness",
+        "Dark secret",
+        "My Fair Godmother",
+        "11/22/63",
+        "Blood of Elves",
+        "The Little Prince",
+        "Winnie the Pooh"
     ]
 });
